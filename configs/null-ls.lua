8fe7d00751aea7328480c6a618d9b8487bfb2a00
Lucio Afonso <lucioafonso@icloud.com>
2023-09-15 22:31:33 +0200

    Enabled null-ls.lua code formatting

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,6 +1,9 @@
 local null_ls = require("null-ls")
 
+local augroup = vim.api.nvim_create_augroup("LSPFormmatting", {})
+
 local b = null_ls.builtins
+
 
 local sources = {
   -- FORMATTING
@@ -40,4 +43,20 @@ local sources = {
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 }
