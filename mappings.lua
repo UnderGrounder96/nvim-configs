22135fe4d5914d6443ffcb8820e4ff70bf981c6b
Lucio Afonso <lucioafonso@icloud.com>
2023-09-15 22:31:08 +0200

    Added movement highlight mapping

--- mappings.lua
+++ mappings.lua
@@ -6,15 +6,18 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts },
   },
+  v = {
+    ["J"] = { ":m '>+1<CR>gv=gv", "Move +highlighted lines", opts },
+    ["K"] = { ":m '<-2<CR>gv=gv", "Move -highlighted lines", opts },
+  },
 }
 
 -- more keybinds!
 M.lspsaga = {
   n = {
-    ["t"] =  { "<cmd>Lspsaga term_toggle<CR>", "enable floating terminal", opts },
-    ["f"] =  { "<cmd>Lspsaga code_action<CR>", "execute code action check", opts },
+    ["t"] = { "<cmd>Lspsaga term_toggle<CR>", "enable floating terminal", opts },
+    ["f"] = { "<cmd>Lspsaga code_action<CR>", "execute code action check", opts },
   },
 }
 
 return M
-
